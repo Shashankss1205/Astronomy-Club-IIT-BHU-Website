5713574f517b36d3030d630b93f143e866a9d4b4
Shashwat <shashwatmaths@gmail.com>
2023-10-27 15:54:38 +0530

    Added images to gallery

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Header from "./Components/Header"
-
+import Footer from "./Components/Header"
 
 export default function Home() {
   return (
+    <div>
     <Header></Header>
-  )
+    <Footer></Footer>
+  </div>)
 }
--- src/app/pages/Gallery/page.js
+++ src/app/pages/Gallery/page.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Header from '../../Components/Header'
 import Gallery from './Gallery'
 
+const images = []
+for (let i = 1; i <= 33; i++) {
+  images.push(`/images/gallery/Image${i}.JPG`); 
+}
+
 const page = () => {
   return (
-    <>
+    <div>
       <Header></Header>
-      <Gallery></Gallery>
-    </>
+      <Gallery images={images}></Gallery>
+    </div>
   )
 }
 
 export default page
+
