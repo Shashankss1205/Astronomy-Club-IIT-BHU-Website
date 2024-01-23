982fb1b3d69bc8a20c87b13a250cfb5d7c92982c
Shashwat <shashwatmaths@gmail.com>
2024-01-23 15:42:37 +0530

    Gallery text

--- src/app/pages/Gallery/Gallery.jsx
+++ src/app/pages/Gallery/Gallery.jsx
@@ -39,6 +39,16 @@ function Gallery({ images }) {
 
   return (
     <div className="bg-black container px-5 py-10 mx-auto">
+       <div className="flex flex-col text-center text-white w-full mb-10">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-black mt-4">
+          Our camera roll
+        </h1>
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-white">
+          This gallery contains pictures from our victories at
+          Inter-IIT and different colleges, Alumni's meet, partying,
+          orientations, workshops, and Astrophotography.
+        </p>
+      </div>
       <div className="columns-1 gap-0 1g:gap- sm:columns-2 lg:columns-3 xl:columns-4 [&>img: not (:first-child)]:mt-5 lg: [&>img:not (:first-child)]=mt-8">
         {images.map((src, index) => (
           <div key={index} className="p-2">
