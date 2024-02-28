c95c866bb2e507b307358262b427f7416d8c7764
Pritish Sharma <pritishsharma96@gmail.com>
2024-02-28 23:59:21 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ export default function page() {
   return (
     <>
         <Home></Home>
-        <Footer></Footer>
     </>
   )
 
--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import Hero from '../../../../public/Hero.png'
 import ParticleEffect from '@/app/Animations/ParticleEffect';
 import { motion } from "framer-motion"
+import Link from 'next/link'
 
 
 const roboto = Roboto({
@@ -35,7 +36,9 @@ const Home = () => {
                     <p className='text-white text-[10px] lg:text-[25px] md:text-[20px] '><span className={roboto.className}>Unveiling the Mysteries of the Universe,</span></p>
                     <p className='text-white text-[10px] lg:text-[25px] md:text-[20px]'><span className={roboto.className}>we are <span className='text-gradient font-bold'>Astronomy</span> club of IIT BHU.</span></p>
                  </div>
+                 <Link href='/pages/Activities'>
                  <button className='bg-gradient-to-r from-white to-slate-400 rounded-[5px] w-[130px] mt-[40px] px-[3px] py-[5px] '> <span className={roboto.className}><span className='font-bold text-[20px]'>explore</span></span></button>
+                 </Link>
             </div>
             <div className='flex items-center justify-center h-screen container w-[45vw] mr-[70px]'>
                 
